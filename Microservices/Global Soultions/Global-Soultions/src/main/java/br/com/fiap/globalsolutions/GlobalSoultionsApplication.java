87db7b9d5446aa2c83fa55fb62c5808e3cc997db
Gandalf The Grey <mcavalcantirebola@gmail.com>
2023-06-05 13:51:53 -0300

    configuração do pacote com spring initialzr

---
+++ Microservices/Global Soultions/Global-Soultions/src/main/java/br/com/fiap/globalsolutions/GlobalSoultionsApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.globalsolutions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlobalSoultionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlobalSoultionsApplication.class, args);
+	}
+
+}
---
+++ Microservices/Global Soultions/Global-Soultions/src/test/java/br/com/fiap/globalsolutions/GlobalSoultionsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.globalsolutions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlobalSoultionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
